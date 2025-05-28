b280bb6b245be1a6aab5c5ffbff0e258dc3fd98a
abreujean <jeandcabreu@gmail.com>
2025-05-28 15:03:12 -0300

    :bug: fix: Ajuste no arquivo de rotas.

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('tasks')->group(function () {
     Route::post('/', [\App\Http\Controllers\TaskController::class, 'createTask'])->name('tasks.create');
     Route::get('/', [\App\Http\Controllers\TaskController::class, 'getTasks'])->name('tasks.index');
-    Route::get('/{hash}', [\App\Http\Controllers\TaskController::class, 'getTask'])->name('tasks.show');
-    Route::put('/{hash}', [\App\Http\Controllers\TaskController::class, 'updateTask'])->name('tasks.update');
+    Route::patch('/{hash}', [\App\Http\Controllers\TaskController::class, 'updateStatus'])->name('tasks.update.status');
     Route::delete('/{hash}', [\App\Http\Controllers\TaskController::class, 'deleteTask'])->name('tasks.delete');
     Route::get('/status/{status}', [\App\Http\Controllers\TaskController::class, 'getTasksByStatus'])->name('tasks.status');
 });
